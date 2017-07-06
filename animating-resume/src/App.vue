82953c203f597d8a024fdeda695308f365ab5501
hungryhailong <1208071427@qq.com>
2017-07-06 21:27:04 +0800

    check error

--- animating-resume/src/App.vue
+++ animating-resume/src/App.vue
@@ -23,10 +23,13 @@
         enableHtml: false,
         fullStyle: [
           `/*
-* Inspired by http://strml.net/
-* 大家好，我是方方 
-* 二月了，好多公司都在招聘，你是不是也在准备简历呀。
-* 说做就做，我也来写一份简历！
+
+* 大家好，我是谯海龙 
+* 最近在学习Vue.js 2，加上秋招即将开始
+* 想用vue做一份在线简历，在腾讯课堂里上了方老师的课
+* 觉得这个效果非常酷，学完自己弄了一份
+* 希望大家能够耐心看完，当做是对我的小小鼓励，谢谢
+* 下面 开始做这个简历
 */
 
 /* 首先给所有元素加上过渡效果 */
@@ -34,42 +37,43 @@
   -webkit-transition: all .3s;
   transition: all .3s;
 }
-/* 白色背景太单调了，我们来点背景 */
+/* 白色背景太单调了，来点背景 */
 html {
-  color: rgb(222,222,222); background: rgb(0,43,54); 
+  color: rgb(222,222,222); background: rgb(1,101,100); 
 }
-/* 文字离边框太近了 */
+/* 文字离边框太近了，稍微调整下 */
 .styleEditor {
   padding: .5em;
   border: 1px solid;
   margin: .5em;
   overflow: auto;
   width: 45vw; height: 90vh;
+  background: rgb(0,43,54);
 }
-/* 代码高亮 */
+/* 给代码加上高亮色 */
 .token.selector{ color: rgb(133,153,0); }
 .token.property{ color: rgb(187,137,0); }
 .token.punctuation{ color: yellow; }
 .token.function{ color: rgb(42,161,152); }
 
-/* 加点 3D 效果呗 */
+/* 弄点并不酷的 3D 效果 */
 html{
   -webkit-perspective: 1000px;
           perspective: 1000px;
 }
 .styleEditor {
-  position: fixed; left: 0; top: 0; 
+  position: fixed; left: 0; top: 0;  
   -webkit-transition: none; 
   transition: none;
   -webkit-transform: rotateY(10deg) translateZ(-100px) ;
           transform: rotateY(10deg) translateZ(-100px) ;
 }
 
-/* 接下来我给自己准备一个编辑器 */
+/* 接下来给自己的简历准备一个编辑器 */
 .resumeEditor{
   position: fixed; right: 0; top: 0;
   padding: .5em;  margin: .5em;
-  width: 48vw; height: 90vh; 
+  width: 48vw; height: 95vh; 
   border: 1px solid;
   background: white; color: #222;
   overflow: auto;
@@ -115,36 +119,82 @@ html{
   padding: .5em;
   background: #ddd;
 }
+.resumeEditor blockquote h3{
+  margin:5px;
+}
+.resumeEditor blockquote ul{
+  text-indent:5px;
+}
+.resumeEditor a{
+    display: block;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    float: left;
+    text-decoration: none;
+    background: rgb(232,221,203);
+    margin-right: 10px;
+    padding: 0 5px;
+    color: #000;
+}
+/*最后再小小调整一下*/
+.resumeEditor{
+    filter:alpha(Opacity=90);-moz-opacity:0.9;opacity: 0.9; 
+    position: fixed; right: 100px; top: 30px;
+    transform: rotateY(-10deg) translateZ(-100px);
+}
+.styleEditor{ 
+  left: 100px; 
+  filter:alpha(Opacity=80);-moz-opacity:0.8;opacity: 0.8;
+}
 `],
         currentMarkdown: '',
-        fullMarkdown: `方应杭
+        fullMarkdown: `
+谯海龙
 ----
 
-资深前端工程师，资深前端讲师，现在在 [饥人谷](http://jirengu.com) 教前端课程。
+* 2018届东北大学学生，信息安全专业，专业成绩排名20%
+* 来自重庆，拥有积极乐观的心态，热爱新鲜事物，敢于尝试，较强的责任。
+* 热爱前端开发，自2016暑假开始接触前端，对前端工作充满热情，对新知识充满学习欲望。
+
 
 技能
 ----
 
-* 前端开发
-* Rails 开发
-* Node.js 开发
-* 前端授课
+* 前端基础开发：掌握HTML(5)，CSS(3),javascript,webpack
+* 前端框架学习：掌握jQuery,Bootstrap,熟悉Vue/Vue2,了解Angular，React
+* 后端语言：熟悉node.js，熟悉php+mysql编程，了解python
+* 计算机掌握：熟悉c++，java等面向对象语言，良好的数据结构和算法设计基础
+* 任职资格：具备良好的团队合作精神，善于沟通，有过项目负责人经验，求知欲强
 
-工作经历
+
+项目经历
 ----
+> ### 校学生成绩管理系统实现                         2016.9至2016.12 
+   * 项目描述：对全院学生成绩进行管理，包括前端录入，反馈和后台数据库管理。
+   * 本人职责：项目成员。负责前端部分页面设计与实现，参与数据库的设计，参与项目前后端交互的实现。
+   * 主要业绩：经过专业老师审批和修改，为院学生使用。
+　
 
-1. [饥人谷](http://jirengu.com)
-2. 腾讯即时通讯平台部
-3. 阿里巴巴B2B部门
-4. 彩程知人项目组
+> ### 个人网页的简单设计与实现                       2016.10
+   * 项目描述：根据个人需求设计网页并通过代码实现。
+   * 本人职责：项目负责人。负责项目的设计，网页的代码实现以及后续的维护。
+   * 主要业绩：该网站已于2016年10月完成，发布到互联网
+>
+
+校园经历
+----
+ * 奖励荣誉：2014-2016年度  获国家励志奖学金、校二三等奖学金，自立自强先进个人等称号。
+ * 教育背景：2014-至今 沈阳东北大学本科就读。
+ * 校内活动：2015-至今  东北大学IBM俱乐部成员，参与各项活动组织和学习讨论。参加各类科技竞赛，包括全国大学生数学建模，信息安全竞赛等。2014-2015年   担任院学生会科技部副部长，参与院内各项科技赛事的组织。
 
 链接
 ----
+ [GitHub](https://github.com/hungryhailong)
+ [学习过程中的案例](https://github.com/hungryhailong/web-project/tree/master/learn-web)
+ [烂烂的gitpage](https://hungryhailong.github.io)
+ [文本简历下载](http://hungryhailong.github.io/files/resume.doc)
 
-* [GitHub](https://github.com/frankfang)
-* [我的文章](https://www.zhihu.com/people/zhihusucks/pins/posts)
-
-> 如果你喜欢这个效果，Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你自己的简历！
 
 `
       }
